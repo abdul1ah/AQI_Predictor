45df261029cd4801843723098686f0d6ac2c3160
abdul1ah <ma2695633@gmail.com>
2026-04-19 03:59:32 +0500

    fixed comma mistake

--- src/feature_pipeline/store_features.py
+++ src/feature_pipeline/store_features.py
@@ -17,7 +17,7 @@ def upload_to_feature_store(features_df: pd.DataFrame):
             
             project = hopsworks.login(
                 project=HOPSWORKS_PROJECT_NAME, 
-                api_key_value=HOPSWORKS_API_KEY
+                api_key_value=HOPSWORKS_API_KEY,
                 hostname_verification=False
             )
             fs = project.get_feature_store()
