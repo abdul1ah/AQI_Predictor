e27bbb6f015f0ceb571b76238b5ea4c25f81b420
abdul1ah <ma2695633@gmail.com>
2026-05-09 13:14:06 +0500

    initialize SHAP explainer for  interpretability

--- src/training_pipeline/train_evaluate.py
+++ src/training_pipeline/train_evaluate.py
@@ -127,7 +127,8 @@ def train_model(train_data: pd.DataFrame) -> Tuple[Dict[str, Any], Dict[str, Dic
 
         try:
             if best_target_name in ["Random_Forest", "XGBoost", "LightGBM"]:
-                explainer = shap.TreeExplainer(best_target_model)
+                best_target_model.set_params(base_score=0.5)
+                Explainer(best_target_model)
                 shap_values = explainer.shap_values(X_test)
                 
                 plt.figure(figsize=(10, 6))
